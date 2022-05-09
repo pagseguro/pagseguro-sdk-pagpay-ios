c712af86dc415ff7af43082f486afe52ca5595fb
andre-bahia <dev.andreluis@gmail.com>
2022-05-09 19:33:01 -0300

    Add app example

---
+++ example/AppExample/AppExample/PagPay.xcframework/ios-arm64/PagPay.framework/Headers/PagPay.h
@@ -0,0 +1,19 @@
+//
+//  PagPay.h
+//  PagPay
+//
+//  Created by Lucas Brito on 11/08/20.
+//  Copyright © 2020 PagBank. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PagPay.
+FOUNDATION_EXPORT double PagPayVersionNumber;
+
+//! Project version string for PagPay.
+FOUNDATION_EXPORT const unsigned char PagPayVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PagPay/PublicHeader.h>
+
+
